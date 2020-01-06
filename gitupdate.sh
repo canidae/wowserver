d9e58f963151409772fcf86bf586b2fedb30eb45
canidae <canidae@exent.net>
2020-01-06 15:09:04 +0100

    fixing git scripts

--- gitupdate.sh
+++ gitupdate.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 git pull
-git submodule update --remote --recursive
 cd build/mangos-classic
-git pull origin pinchcliffe
+git pull upstream master
+git merge --no-edit origin/pinchcliffe
+git merge --no-edit origin/new_ahbot
 cd -
 cd files/cmangos/classic-db
-git pull origin pinchcliffe
+git pull upstream master
+git merge --no-edit origin/pinchcliffe
 cd -
