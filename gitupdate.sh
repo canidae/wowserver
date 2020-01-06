8c2469737d610381b2eecbff43cbb29330ee1397
canidae <canidae@exent.net>
2020-01-06 15:11:36 +0100

    --no-edit on pull as well

--- gitupdate.sh
+++ gitupdate.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 git pull
 cd build/mangos-classic
-git pull upstream master
+git pull --no-edit upstream master
 git merge --no-edit origin/pinchcliffe
 git merge --no-edit origin/new_ahbot
 cd -
 cd files/cmangos/classic-db
-git pull upstream master
+git pull --no-edit upstream master
 git merge --no-edit origin/pinchcliffe
 cd -
