376c50074e9dc33d1661b79434ab34180075a036
Vidar Wahlberg <canidae@exent.net>
2020-01-06 16:33:44 +0100

    checkout detached head before merging in gitupdate.sh

--- gitupdate.sh
+++ gitupdate.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 git pull
 cd build/mangos-classic
+git checkout --detach
 git pull --no-edit upstream master
 git merge --no-edit origin/pinchcliffe
 git merge --no-edit origin/new_ahbot
 cd -
 cd files/cmangos/classic-db
+git checkout --detach
 git pull --no-edit upstream master
 git merge --no-edit origin/pinchcliffe
 cd -
