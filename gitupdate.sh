26eecd11589c329d686ae6b0dcc7c817ac073aa8
Vidar Wahlberg <canidae@exent.net>
2020-01-09 12:07:34 +0100

    new_ahbot -> ahbot

--- gitupdate.sh
+++ gitupdate.sh
@@ -4,7 +4,7 @@ cd build/mangos-classic
 git checkout --detach
 git pull --no-edit upstream master
 git merge --no-edit origin/pinchcliffe
-git merge --no-edit origin/new_ahbot
+git merge --no-edit origin/ahbot
 cd -
 cd files/cmangos/classic-db
 git checkout --detach
