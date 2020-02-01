db84e5a96bcb39793044145806eeee090d7755fa
Vidar Wahlberg <canidae@exent.net>
2020-02-01 14:29:55 +0100

    updated dockerfile

--- gitupdate.sh
+++ gitupdate.sh
@@ -5,6 +5,7 @@ git checkout --detach
 git pull --no-edit upstream master
 git merge --no-edit origin/pinchcliffe
 git merge --no-edit origin/ahbot
+git merge --no-edit origin/create_db_if_not_exists
 cd -
 cd files/cmangos/classic-db
 git checkout --detach
