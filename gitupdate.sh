f6a054a81d8bec6e39ad62a03ac763db61ba1d1b
Vidar Wahlberg <canidae@exent.net>
2020-02-01 14:33:51 +0100

    fetch origin & upstream before merging

--- gitupdate.sh
+++ gitupdate.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 git pull
 cd build/mangos-classic
+git fetch origin
+git fetch upstream
 git checkout --detach
 git pull --no-edit upstream master
 git merge --no-edit origin/pinchcliffe
@@ -8,6 +10,8 @@ git merge --no-edit origin/ahbot
 git merge --no-edit origin/create_db_if_not_exists
 cd -
 cd files/cmangos/classic-db
+git fetch origin
+git fetch upstream
 git checkout --detach
 git pull --no-edit upstream master
 git merge --no-edit origin/pinchcliffe
