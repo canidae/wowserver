7eb5050ff6fbdfa9445d7f4f968ece5d8b7bfe29
Vidar Wahlberg <canidae@exent.net>
2020-02-04 17:46:12 +0100

    updated initing submodules

--- gitinit.sh
+++ gitinit.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
-cd build/mangos-classic
 git submodule init
-git submodule update
+git submodule update --no-fetch
+cd build/mangos-classic
 git remote add upstream https://github.com/cmangos/mangos-classic.git
 cd -
 cd files/cmangos/classic-db
-git submodule init
-git submodule update
 git remote add upstream https://github.com/cmangos/classic-db.git
 cd -
