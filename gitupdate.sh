96cb16276ebc2414a2c56d31c037c3004bdd937b
Vidar Wahlberg <canidae@exent.net>
2020-03-07 14:39:53 +0100

    don't need this branch anymore

--- gitupdate.sh
+++ gitupdate.sh
@@ -7,7 +7,6 @@ git checkout --detach
 git pull --no-edit upstream master
 git merge --no-edit origin/pinchcliffe
 git merge --no-edit origin/ahbot
-git merge --no-edit origin/create_db_if_not_exists
 cd -
 cd src/classic-db
 git fetch origin
