6a91858a1e67acf9088726551dff4e1a19f322ed
Vidar Wahlberg <canidae@exent.net>
2020-05-18 18:23:03 +0200

    using ssh instead of https

--- gitinit.sh
+++ gitinit.sh
@@ -2,8 +2,8 @@
 git submodule init
 git submodule update --no-fetch
 cd src/mangos-classic
-git remote add upstream https://github.com/cmangos/mangos-classic.git
+git remote add upstream git@github.com:cmangos/mangos-classic.git
 cd -
 cd src/classic-db
-git remote add upstream https://github.com/cmangos/classic-db.git
+git remote add upstream git@github.com:cmangos/classic-db.git
 cd -
