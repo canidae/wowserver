1d1013abcee4659b3f8f99f17fbae8ef0fd3451c
Vidar Wahlberg <canidae@exent.net>
2020-07-20 22:15:50 +0200

    ahbot merged into upstream

--- gitupdate.sh
+++ gitupdate.sh
@@ -6,7 +6,6 @@ git fetch upstream
 git checkout --detach
 git pull --no-edit upstream master
 git merge --no-edit origin/pinchcliffe
-git merge --no-edit origin/ahbot
 cd -
 cd src/classic-db
 git fetch origin
