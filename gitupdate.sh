724dcbf7abdd4b9a73a98110ca33393606d9228a
Vidar Wahlberg <canidae@exent.net>
2020-09-09 17:51:39 +0200

    valgrind_fixes merged, added branch ahbot_items

--- gitupdate.sh
+++ gitupdate.sh
@@ -7,7 +7,6 @@ git checkout master
 git checkout --detach
 git pull --no-edit upstream master
 git merge --no-edit origin/pinchcliffe
-git merge --no-edit origin/valgrind_fixes
 cd -
 cd src/classic-db
 git fetch origin
@@ -16,4 +15,5 @@ git checkout master
 git checkout --detach
 git pull --no-edit upstream master
 git merge --no-edit origin/pinchcliffe
+git merge --no-edit origin/ahbot_items
 cd -
