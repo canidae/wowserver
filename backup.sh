535e98e1423f97a2f836186ea4e79cb8c339b372
Vidar Wahlberg <canidae@exent.net>
2020-11-23 16:11:54 +0100

    "/" wasn't supposed to be there

--- backup.sh
+++ backup.sh
@@ -2,6 +2,6 @@
 cd /root/wowserver
 mount /dev/sdb1 /mnt/
 docker-compose exec cmangos bash -c "mysqldump --all-databases > databases.sql"
-tar cpzf "/mnt/$(date +"%y%m%d").tar.gz" /src/databases.sql
+tar cpzf "/mnt/$(date +"%y%m%d").tar.gz" src/databases.sql
 sync
 umount /mnt
