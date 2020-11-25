f9d1ea1a6cd36a38183a7aa910aa43455bb9130a
Vidar Wahlberg <canidae@exent.net>
2020-11-25 17:46:31 +0100

    upgraded ubuntu, merging new branches

--- gitupdate.sh
+++ gitupdate.sh
@@ -7,6 +7,8 @@ git checkout master
 git checkout --detach
 git pull --no-edit upstream master
 git merge --no-edit origin/pinchcliffe
+git merge --no-edit origin/faerie_fix
+git merge --no-edit origin/new_mysql
 cd -
 cd src/classic-db
 git fetch origin
