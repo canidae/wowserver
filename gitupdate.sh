25f077bd0d72cf0b14f591dca4c0ada09368f594
Vidar Wahlberg <canidae@exent.net>
2020-12-13 16:53:41 +0100

    added branch for another minor fix

--- gitupdate.sh
+++ gitupdate.sh
@@ -8,6 +8,7 @@ git checkout --detach
 git pull --no-edit upstream master
 git merge --no-edit origin/pinchcliffe
 git merge --no-edit origin/faerie_fix
+git merge --no-edit origin/stopresting
 git merge --no-edit origin/new_mysql
 cd -
 cd src/classic-db
