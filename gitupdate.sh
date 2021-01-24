0f4583db28afe54cfab0f72e2774ac94034238b2
Vidar Wahlberg <canidae@exent.net>
2021-01-24 16:43:33 +0100

    cleanup gitupdate.sh

--- gitupdate.sh
+++ gitupdate.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-git pull
 cd src/mangos-wotlk
 git fetch canidae
 git fetch origin
@@ -8,9 +7,6 @@ git checkout --detach
 git pull --no-edit origin master
 git merge --no-edit canidae/new_mysql
 git merge --no-edit canidae/ahbot_balancing
-#git merge --no-edit canidae/pinchcliffe
-#git merge --no-edit canidae/faerie_fix
-#git merge --no-edit canidae/stopresting
 cd -
 cd src/wotlk-db
 git fetch canidae
@@ -18,7 +14,4 @@ git fetch origin
 git checkout master
 git checkout --detach
 git pull --no-edit origin master
-git merge --no-edit canidae/disrupt_their_reinforcements
-#git merge --no-edit canidae/pinchcliffe
-#git merge --no-edit canidae/felsteed
 cd -
