bb1dd0d60b1a485dde4e52265b16fb05829da3f4
Vidar Wahlberg <canidae@exent.net>
2021-02-01 17:35:53 +0100

    Disable pseudo-tty allocation

--- backup.sh
+++ backup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd /root/wowserver
 rm src/wotlk.sql
-docker-compose exec cmangos bash -c "mysqldump --all-databases > wotlk.sql"
+docker-compose exec -T cmangos bash -c "mysqldump --all-databases > wotlk.sql"
 mount /dev/sdb1 /mnt/
 tar cpzf "/mnt/$(date +"%y%m%d").tar.gz" src/wotlk.sql
 sync
